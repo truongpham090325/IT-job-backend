78dfbeed9cd2d5a5a7452cb3a17a395c68743e7d
Phạm Quang Trường <truongpham090325@gmail.com>
2026-02-20 16:55:42 +0700

    Tính năng Đăng nhập (Nhà tuyển dụng)

--- controllers/company.controller.ts
+++ controllers/company.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import AccountCompany from "../model/account-company.model";
+import jwt from "jsonwebtoken";
 
 export const registerPost = async (req: Request, res: Response) => {
   try {
@@ -27,6 +28,64 @@ export const registerPost = async (req: Request, res: Response) => {
     res.json({
       code: "success",
       message: "Đăng ký tài khoản thành công!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      code: "error",
+      message: "Dữ liệu không hợp lệ!",
+    });
+  }
+};
+
+export const loginPost = async (req: Request, res: Response) => {
+  try {
+    const existAccount = await AccountCompany.findOne({
+      email: req.body.email,
+    });
+
+    if (!existAccount) {
+      res.json({
+        code: "error",
+        message: "Tài khoản không tồn tại trong hệ thống!",
+      });
+      return;
+    }
+
+    const isPassword = bcrypt.compareSync(
+      req.body.password,
+      `${existAccount.password}`,
+    );
+
+    if (!isPassword) {
+      res.json({
+        code: "error",
+        message: "Mật khẩu không chính xác!",
+      });
+      return;
+    }
+
+    const tokenCompany = jwt.sign(
+      {
+        id: existAccount.id,
+        email: existAccount.email,
+      },
+      `${process.env.JWT_SECRET}`,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    res.cookie("tokenCompany", tokenCompany, {
+      maxAge: 24 * 60 * 60 * 1000, // 1 ngày
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // https để true, http để fasle
+      sameSite: "lax",
+    });
+
+    res.json({
+      code: "success",
+      message: "Đăng nhập thành công!",
     });
   } catch (error) {
     console.log(error);
